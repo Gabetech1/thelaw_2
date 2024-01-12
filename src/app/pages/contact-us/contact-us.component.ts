8186bc2c144adaf62e40a1a926ad7b95bac9f406
Gabriel Abiah <gabriel@manifestghana.com>
2024-01-12 16:04:24 +0000

    fixing contact form

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      // Add more form controls as needed
+    });
+  }
+
+  onSubmit() {
+    console.log(this.form);
+    if (this.form.valid) {
+      const formData = this.form.value;
+
+      // this.submitFormData(formData);
+    }
+  }
+
+  private submitFormData(formData: any) {
+    // Replace 'your-api-endpoint' with the actual API endpoint
+    const apiUrl = 'your-api-endpoint';
+
+    /*  this.http.post(apiUrl, formData).subscribe(
+      (response) => {
+        console.log('Form submitted successfully:', response);
+      },
+      (error) => {
+        console.error('Error submitting form:', error);
+      }
+    ); */
+  }
+}
